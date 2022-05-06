5ff6974f3f9e62ddc8036bf4be06f4a16d29b83d
Md Shahriyar Alam <mdshahriyaralam552@gmail.com>
2022-05-06 11:55:42 +0600

    Initial Project Setup

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function App() {
+  return (
+    <div>
+      <h1 className='text-2xl font-bold text-green-500'>Hello World</h1>
+    </div>
+  )
+}
+
+export default App
