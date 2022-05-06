137942803b2999f70aa351f9e59714e14816a494
Md Shahriyar Alam <mdshahriyaralam552@gmail.com>
2022-05-06 15:48:54 +0600

    Refractor components pages and sectitons

--- src/sections/Products/Products.jsx
+++ src/sections/Products/Products.jsx
@@ -1,7 +1,7 @@
 import React from 'react'
 import { Link } from 'react-router-dom'
 import Container from '../../layouts/Container'
-import Product from '../../Product/Product'
+import Product from '../../components/Product/Product'
 
 const Products = () => {
   return (
