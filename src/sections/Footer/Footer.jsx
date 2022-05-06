5e492e624775326a311229f96ec6afe7099c6fc8
Md Shahriyar Alam <mdshahriyaralam552@gmail.com>
2022-05-06 19:59:49 +0600

    Made footer responsive

--- src/sections/Footer/Footer.jsx
+++ src/sections/Footer/Footer.jsx
@@ -5,8 +5,8 @@ import Container from '../../components/Container/Container'
 const Footer = () => {
   return (
     <div>
-      <Container className='py-10 grid grid-cols-4 gap-5'>
-        <div className='col-span-2'>
+      <Container className='py-20 grid grid-cols-1 sm:grid-cols-2 md:grid-cols-4 gap-5'>
+        <div className='col-span-1 sm:col-span-2'>
           <h1 className='text-2xl font-bold uppercase'>Laptop Carnival</h1>
           <p className='text-zinc-500'>Our warehouse has the largest collection of laptops from all the brands in existance. Check it out.</p>
         </div>
