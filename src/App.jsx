4e72226c0c6d7463658c1934f21d8f96804429b3
Md Shahriyar Alam <mdshahriyaralam552@gmail.com>
2022-05-06 13:31:25 +0600

    Added login page

--- src/App.jsx
+++ src/App.jsx
@@ -3,12 +3,14 @@ import { Routes, Route } from "react-router-dom";
 
 import Layout from './layouts/Layout'
 import Home from './pages/Home';
+import Login from './pages/Login';
 
 function App() {
   return (
     <Layout>
       <Routes>
         <Route path="/" element={<Home />} />
+        <Route path="/login" element={<Login />} />
       </Routes>
     </Layout>
   )
---
+++ src/pages/Login.jsx
@@ -0,0 +1,39 @@
+import React from 'react'
+import Container from '../layouts/Container'
+import { useForm } from 'react-hook-form'
+import { FcGoogle } from 'react-icons/fc'
+
+const Login = () => {
+  const { register, handleSubmit, formState: { errors } } = useForm();
+  const onSubmit = data => console.log(data);
+
+  return (
+    <div>
+      <Container className='py-20'>
+        <h1 className='text-3xl font-bold text-center mb-5'>Login</h1>
+
+        <form onSubmit={handleSubmit(onSubmit)} className='max-w-[400px] mx-auto'>
+          <div className='flex flex-col gap-5'>
+            <div className='flex flex-col gap-1'>
+              <label>Email</label>
+              <input type='email' {...register("email", { required: true })} />
+              {errors.email && <span className='text-xs text-red-400'>This field is required</span>}
+            </div>
+
+            <div className='flex flex-col gap-2'>
+              <label>Password</label>
+              <input type='password' {...register("password", { required: true })} />
+              {errors.password && <span className='text-xs text-red-400'>This field is required</span>}
+            </div>
+          </div>
+          <div className='grid grid-cols-3 gap-4 mt-5'>
+            <input className='py-3 bg-indigo-500 text-white rounded-md cursor-pointer' type="submit" value='Login' />
+            <button type='button' className='py-3 bg-black col-span-2 flex items-center gap-2 justify-center rounded-md text-white'><FcGoogle className='text-2xl' /> Google</button>
+          </div>
+        </form>
+      </Container>
+    </div>
+  )
+}
+
+export default Login
