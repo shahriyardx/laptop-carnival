fabf60bcf39e51ec9d48e76f4194af060e2d0537
Md Shahriyar Alam <mdshahriyaralam552@gmail.com>
2022-05-06 12:28:09 +0600

    Added padding on contatiner

--- src/layouts/Container.jsx
+++ src/layouts/Container.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 
 const Container = ({ className, children, ...props }) => {
   return (
-    <div className={`container mx-auto ${className}`} {...props}>
+    <div className={`container mx-auto px-4 md:px-0 ${className}`} {...props}>
         {children}
     </div>
   )
