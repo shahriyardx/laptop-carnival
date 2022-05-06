d11e77f1bcd9639a7e5d2cd6d4251679a65678a3
Md Shahriyar Alam <mdshahriyaralam552@gmail.com>
2022-05-06 13:35:15 +0600

    Added register page

--- src/App.jsx
+++ src/App.jsx
@@ -4,6 +4,7 @@ import { Routes, Route } from "react-router-dom";
 import Layout from './layouts/Layout'
 import Home from './pages/Home';
 import Login from './pages/Login';
+import Register from './pages/Register';
 
 function App() {
   return (
@@ -11,6 +12,7 @@ function App() {
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/login" element={<Login />} />
+        <Route path="/register" element={<Register />} />
       </Routes>
     </Layout>
   )
--- src/pages/Register.jsx
+++ src/pages/Register.jsx
@@ -4,14 +4,14 @@ import { useForm } from 'react-hook-form'
 import { FcGoogle } from 'react-icons/fc'
 import { Link } from 'react-router-dom'
 
-const Login = () => {
+const Register = () => {
   const { register, handleSubmit, formState: { errors } } = useForm();
   const onSubmit = data => console.log(data);
 
   return (
     <div>
       <Container className='py-20'>
-        <h1 className='text-3xl font-bold text-center mb-5'>Login</h1>
+        <h1 className='text-3xl font-bold text-center mb-5'>Register</h1>
 
         <form onSubmit={handleSubmit(onSubmit)} className='max-w-[400px] mx-auto'>
           <div className='flex flex-col gap-5'>
@@ -27,12 +27,10 @@ const Login = () => {
               {errors.password && <span className='text-xs text-red-400'>This field is required</span>}
             </div>
           </div>
-          <div className='grid grid-cols-3 gap-4 mt-5'>
-            <input className='py-3 bg-indigo-500 text-white rounded-md cursor-pointer' type="submit" value='Login' />
-            <button type='button' className='py-3 bg-black col-span-2 flex items-center gap-2 justify-center rounded-md text-white'><FcGoogle className='text-2xl' /> Google</button>
-          </div>
+          
+          <input className='px-4 py-3 bg-indigo-500 text-white rounded-md cursor-pointer mt-5' type="submit" value='Register' />
 
-          <Link to='/register' className='block text-blue-500 mt-5'>Don't have an account?</Link>
+          <Link to='/login' className='block text-blue-500 mt-5'>Already have an account?</Link>
           <Link to='/reset' className='block text-blue-500'>Forgot Password?</Link>
         </form>
       </Container>
@@ -40,4 +38,4 @@ const Login = () => {
   )
 }
 
-export default Login
+export default Register
