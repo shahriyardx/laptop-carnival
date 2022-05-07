9a9af10f2dffc5e1d39486c7bacba8e2322714b7
Md Shahriyar Alam <mdshahriyaralam552@gmail.com>
2022-05-07 21:00:35 +0600

    Load inventory from database

--- src/pages/inventory/Inventory.jsx
+++ src/pages/inventory/Inventory.jsx
@@ -1,9 +1,24 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import Container from '../../components/Container/Container'
 import { BiTrashAlt } from 'react-icons/bi'
 import { Link } from 'react-router-dom'
+import { API_URL } from '../../../config'
+import axios from 'axios'
 
 const Inventory = () => {
+  const [items, setItems] = useState([])
+
+  useEffect(() => {
+    const fetchItems = async () => {
+      const { data } = await axios.get(`${API_URL}/inventory`)
+      setItems(data.items)
+      console.log(data.items)
+    }
+
+    fetchItems()
+      .catch(console.error)
+  }, [])
+
   return (
     <div>
       <Container className='py-20'>
@@ -21,18 +36,20 @@ const Inventory = () => {
           </thead>
 
           <tbody>
-            <tr className='odd:bg-zinc-200'>
-              <td className='px-5 py-2'>1</td>
-              <td className='px-5 py-2'>Macbook Pro 2022</td>
-              <td className='px-5 py-2'>Apple</td>
-              <td className='px-5 py-2'>In Stock</td>
-              <td className='flex flex-wrap gap-2 items-center pl-5 py-2'>
-                <div className='p-2 bg-red-400 text-white text-xl rounded-full cursor-pointer'>
-                  <BiTrashAlt />
-                </div>
-                <Link className='p-2 bg-indigo-400 hover:bg-indigo-500 rounded-md text-white' to='/inventory/1'>Manage</Link>
-              </td> 
-            </tr>
+            {items.map((item, index) => {
+              return (<tr key={item._id} className='odd:bg-zinc-200'>
+                <td className='px-5 py-2'>{index + 1}</td>
+                <td className='px-5 py-2'>{item.title}</td>
+                <td className='px-5 py-2'>{item.brand}</td>
+                <td className='px-5 py-2'>{item.quantity ? 'In Stock' : 'Out Of Stock'}</td>
+                <td className='flex flex-wrap gap-2 items-center pl-5 py-2'>
+                  <div className='p-2 bg-red-400 text-white text-xl rounded-full cursor-pointer'>
+                    <BiTrashAlt />
+                  </div>
+                  <Link className='p-2 bg-indigo-400 hover:bg-indigo-500 rounded-md text-white' to={`/inventory/${item._id}`}>Manage</Link>
+                </td> 
+              </tr>)
+            })}
           </tbody>
         </table>
       </Container>
