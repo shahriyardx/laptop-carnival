4d1ebee92911e22d81f0169e3e0dc602e7a4d80a
Md Shahriyar Alam <mdshahriyaralam552@gmail.com>
2022-05-08 17:18:44 +0600

    Show links only when logged in

--- src/sections/Header/Header.jsx
+++ src/sections/Header/Header.jsx
@@ -23,14 +23,17 @@ const Header = () => {
 
         <div className={`z-50 absolute sm:static top-16 left-0 w-full md:w-auto px-4 py-5 sm:p-0 bg-black sm:bg-transparent text-white sm:text-black flex flex-col sm:flex-row sm:items-center sm:ml-auto gap-2 md:gap-4 ${open ? 'flex' : 'hidden sm:flex'}`}>
           <CustomLink to='/'>Home</CustomLink>
-          <CustomLink to='/inventory'>Manage Inventory</CustomLink>
-          <CustomLink to='/inventory/my'>My Items</CustomLink>
-          <CustomLink to='/inventory/add'>Add Item</CustomLink>
           {user ? (
-            <div className='flex gap-2 items-center'>
-              <span className='font-bold hidden'>{user.displayName}</span>
-              <button className='cursor-pointer px-3 py-2 rounded-md bg-red-500 text-white' onClick={() => signOut(auth)}>Logout</button>
-            </div>
+            <>
+              <CustomLink to='/inventory'>Manage Inventory</CustomLink>
+              <CustomLink to='/inventory/my'>My Items</CustomLink>
+              <CustomLink to='/inventory/add'>Add Item</CustomLink>
+              
+              <div className='flex gap-2 items-center'>
+                <span className='font-bold hidden'>{user.displayName}</span>
+                <button className='cursor-pointer px-3 py-2 rounded-md bg-red-500 text-white' onClick={() => signOut(auth)}>Logout</button>
+              </div>
+            </>
             ) : (
             <CustomLink to='/login' className='px-3 py-2 rounded-md bg-indigo-500 text-white'>Login</CustomLink>
           )}
